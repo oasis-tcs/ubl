37f12bfb029cddee1d0ef4b881b4fe76a5caf8f3
Kenneth Bengtsson <66590520+bdxhub@users.noreply.github.com>
2025-07-24 22:51:44 +0200

    Updated debit note example

--- raw/val/testsamples.sh
+++ raw/val/testsamples.sh
@@ -4,7 +4,7 @@ sh validate.sh ../xsdrt/maindoc/UBL-BusinessCard-$UBLversion.xsd ../xml/UBL-Busi
 sh validate.sh ../xsdrt/maindoc/UBL-CommonTransportationReport-$UBLversion.xsd ../xml/UBL-CommonTransportationReport-2.3-Example.xml
 sh validate.sh ../xsdrt/maindoc/UBL-CreditNote-$UBLversion.xsd ../xml/UBL-CreditNote-2.0-Example.xml
 sh validate.sh ../xsdrt/maindoc/UBL-CreditNote-$UBLversion.xsd ../xml/UBL-CreditNote-2.1-Example.xml
-sh validate.sh ../xsdrt/maindoc/UBL-DebitNote-$UBLversion.xsd ../xml/UBL-DebitNote-2.1-Example.xml
+sh validate.sh ../xsdrt/maindoc/UBL-DebitNote-$UBLversion.xsd ../xml/UBL-DebitNote-2.5-Example.xml
 sh validate.sh ../xsdrt/maindoc/UBL-DespatchAdvice-$UBLversion.xsd ../xml/UBL-DespatchAdvice-2.0-Example.xml
 sh validate.sh ../xsdrt/maindoc/UBL-DigitalAgreement-$UBLversion.xsd ../xml/UBL-DigitalAgreement-2.2-Example-Multilateral.xml
 sh validate.sh ../xsdrt/maindoc/UBL-DigitalAgreement-$UBLversion.xsd ../xml/UBL-DigitalAgreement-2.2-Example.xml
