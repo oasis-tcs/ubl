807949319b5393eb4acac9c2fd7f0e31959b63f9
Kees Duvekot <kduvekot@users.noreply.github.com>
2025-10-14 10:29:42 +0200

    debug build-common.sh

--- build-common.sh
+++ build-common.sh
@@ -14,7 +14,10 @@ echo Building package...
 java -Dant.home=utilities/ant -classpath "utilities/saxon/saxon.jar:utilities/ant/lib/ant-launcher.jar:utilities/saxon9he/saxon9he.jar" org.apache.tools.ant.launch.Launcher -buildfile build.xml "-Dtitle=$title" "-Dpackage=$package" "-DUBLversion=$UBLversion" "-DUBLprevStageVersion=$UBLprevStageVersion" "-DUBLprevStage=$UBLprevStage" "-DUBLprevVersion=$UBLprevVersion" "-Drawdir=$rawdir" "-DlibraryGoogle=$libGoogle" "-DdocumentsGoogle=$docGoogle" "-DsignatureGoogle=$sigGoogle" "-Ddir=$targetdirabs" "-DUBLstage=$UBLstage" "-Dlabel=$label" "-DisDraft=$isDraft" "-Drealtauser=$4" "-Drealtapass=$5" "-Dplatform=$platform"
 serverReturn=$?
 sleep 2
-
+#
+# temporary exit here .. to see the output of the ant build
+#
+exit 0 # always be successful so that github returns ZIP of results
 
 if [ ! -d $targetdir/$package-$UBLstage-$label-archive-only/ ]; then mkdir $targetdir/$package-$UBLstage-$label-archive-only/ ; fi
 mv build.console.$label.txt $targetdir/$package-$UBLstage-$label-archive-only/
